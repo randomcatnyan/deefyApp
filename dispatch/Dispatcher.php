e80f38bfdb1333bd78d327873b3d3e2bd25d28ed
steelOwl69 <>
2025-10-03 17:33:29 +0200

    ex 2

--- dispatch/Dispatcher.php
+++ dispatch/Dispatcher.php
@@ -4,4 +4,47 @@ namespace iutnc\deefy\dispatch;
 
 class AddPlaylistAction extends Action {
 
+    private ?string $action = null;
+
+    public function __construct(){
+
+        $this->action = $_GET[’action’];
+
+    }
+
+    public function run(): void{
+        $q = null;
+        switch($this->action) {
+            case "playlist":
+                $q = new DisplayPlaylistAction();
+            case "add-playlist":
+                $q = new AddPlaylistAction();
+            case "add-track":
+                $q = new AddPodcastTrackAction();
+            default:
+                $q = new DefaultAction();
+        }
+        $this->renderPage($q.execute());
+    }
+
+    public function renderPage(string $html): void{
+
+        echo"
+        <!DOCTYPE html>
+        <html lang='en'>
+        <head>
+        <meta charset='UTF-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>deefy l'app</title>
+        </head>
+        <body>
+        " . $html . "
+        </body>
+        </html>
+        ";
+
+
+    }
+
 }
