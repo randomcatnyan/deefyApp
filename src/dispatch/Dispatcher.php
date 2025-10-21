a7c11519dfb534388c55975feeace7dae36249f9
steelOwl69 <>
2025-10-21 11:17:15 +0200

    add favicon

--- src/dispatch/Dispatcher.php
+++ src/dispatch/Dispatcher.php
@@ -49,7 +49,8 @@ class Dispatcher {
                 <meta charset='UTF-8'>
                 <meta http-equiv='X-UA-Compatible' content='IE=edge'>
                 <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-                <title>deefy l'app</title>
+                <title>deefy app</title>
+                <link rel='icon' href='favicon.png' />
             </head>
             <body>
                 <div>
