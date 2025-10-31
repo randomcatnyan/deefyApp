bf8b125f4f2cf71abcea93ef1b2818eca3a83467
steelOwl69 <>
2025-10-31 18:28:18 +0100

    peux seulement ajouter une playlist quand logged in

--- src/actions/AddPlaylistAction.php
+++ src/actions/AddPlaylistAction.php
@@ -5,6 +5,7 @@ require_once 'vendor/autoload.php';
 use iutnc\deefy\entity\Playlist;
 use iutnc\deefy\render\AudioListRenderer;
 use iutnc\deefy\repository\DeefyRepository;
+use iutnc\deefy\auth\AuthProvider;
 
 class AddPlaylistAction extends Action {
 
@@ -21,17 +22,19 @@ class AddPlaylistAction extends Action {
     }
 
     public function executePost() : string{
+        $to_return = "";
+        if ( AuthProvider::isLoggedIn() ) {
+            $db = DeefyRepository::getInstance();
 
-        $db = DeefyRepository::getInstance();
+            $playlist_name = filter_var($_POST['name'], FILTER_SANITIZE_SPECIAL_CHARS);
 
-        $playlist_name = filter_var($_POST['name'], FILTER_SANITIZE_SPECIAL_CHARS);
-
-        $playlist = new Playlist($playlist_name);
-        $db->saveEmptyPlaylist($playlist);
-        $playlist_id = $playlist->getID();
-        $playlistRenderer = new AudioListRenderer($playlist);
-        $r = "<p>" . $playlistRenderer->render() . "</p><a href='?action=add-track&playlist_id=$playlist_id'><p>Add tracks</p></a>";
-        return $r;
+            $playlist = new Playlist($playlist_name);
+            $db->saveEmptyPlaylist($playlist);
+            $playlist_id = $playlist->getID();
+            $playlistRenderer = new AudioListRenderer($playlist);
+            $to_return = "<p>" . $playlistRenderer->render() . "</p><a href='?action=add-track&playlist_id=$playlist_id'><p>Add tracks</p></a>";
+        }
+        return $to_return;
     }
 
 }
